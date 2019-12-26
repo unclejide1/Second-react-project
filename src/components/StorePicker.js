540ca90fabe560d8e224456ed3dbb9e157d3fcae
Jideofor Nwoke <njusticej@gmail.com>
2019-12-26 14:46:01 +0100

    add storepicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -6,7 +6,7 @@ class StorePicker extends React.Component{
             
         <form className="store-selector">
             <h2>Please Enter A Store</h2>
-            <input type="text" required placeholder=" store name"/>
+            <input type="text" required placeholder=" Store Name"/>
             <button type ="submit"> Visit Store</button>
         </form>
         
