11ab4f6e11d1ac30700ae0a88a8eaa6bd95bcb74
Jideofor Nwoke <njusticej@gmail.com>
2019-12-27 00:46:48 +0100

    add ref, eventlisteners and route to link to apps page on submit of form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,28 @@
-import React, {Fragment} from 'react'
+import React from 'react'
+import {getFunName} from '../helpers'
 
 class StorePicker extends React.Component{
+    myInput = React.createRef();
+
+    goToStore = event =>{
+        //1. stop the form from submitting
+        event.preventDefault()
+        //2 get the text from the input
+        const storename= this.myInput.current.value
+        //3 change the page to /store/whatever-they-entered
+        this.props.history.push(`/store/${storename}`)
+        
+    }
     render () {
         return (
             
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
             <h2>Please Enter A Store</h2>
-            <input type="text" required placeholder=" Store Name"/>
+            <input type="text"
+            ref={this.myInput}
+            required 
+            placeholder=" Store Name"
+            defaultValue= {getFunName()}/>
             <button type ="submit"> Visit Store</button>
         </form>
         
