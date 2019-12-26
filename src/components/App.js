d663ff5d68a27b726dc2fd6b262281f844ece953
Jideofor Nwoke <njusticej@gmail.com>
2019-12-26 14:13:38 +0100

    add order and inventory components, made header component dynamic

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react"
+import Header from './Header'
+import Inventory from './Inventory'
+import Order from './Order'
 
 class App extends React.Component {
     render() {
         return (
             <div className ="catch-of-the-day">
-                <p>heyy</p>
+                <div className="menu">
+                    <Header tagline ="Fresh Seafood Market"/>
+                </div>
+                <Order/>
+                <Inventory/>
             </div>
         );
     }
