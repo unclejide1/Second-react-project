7de2ac6b1926f7b1d21aab464651ccaf90b6a739
Jideofor Nwoke <njusticej@gmail.com>
2020-01-05 00:07:20 +0100

    add addToOrder Function to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -24,17 +24,28 @@ class App extends React.Component {
     loadSampleFishes = () => {
         this.setState({fishes:sampleFishes})
     }
+
+    addToOrder = (key) => {
+        //1. take a copy of state
+        const order = {...this.state.order}
+        //2. either add to the order, or update the number in our order
+        order[key]=order[key] + 1 || 1;
+        //3. call setstate to update our stateobject
+        this.setState({
+            order: order
+        })
+    }
     render() {
         return (
             <div className ="catch-of-the-day">
                 <div className="menu">
                     <Header tagline ="Fresh Seafood Market"/>
                     <ul className="fishes">
-                    {Object.keys(this.state.fishes).map(key => <Fish key = {key} details={this.state.fishes[key]}/>)}
+                    {Object.keys(this.state.fishes).map(key => <Fish key = {key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)}
                     </ul>
                 </div>
-                <Order/>
-                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+                <Order fishes={this.state.fishes} order={this.state.order}/>
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         );
     }
