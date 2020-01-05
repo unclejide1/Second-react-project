075427c10821fb0977235b91607d5c52b529099f
Jideofor Nwoke <njusticej@gmail.com>
2020-01-06 00:39:55 +0100

    add lifecycle methods, componentDidMount and componentWillUnmount to sync data to firebase

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,24 @@ import Inventory from './Inventory'
 import Order from './Order'
 import sampleFishes from '../sample-fishes'
 import Fish from './Fish'
+import base from '../base'
+
 
 class App extends React.Component {
     state={
         fishes: {},
         order: {}
+    }
+    componentDidMount() {
+        const { params } = this.props.match
+        this.ref = base.syncState(`${params.storeId}/fishes`, {
+            context: this,
+            state: "fishes"
+          });
+    }
+
+    componentWillUnmount(){
+        base.removeBinding(this.ref)
     }
     addFish = fish => {
         //1. take a copy of the existing state.
