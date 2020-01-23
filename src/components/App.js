b944b2c9698b9fe78f9f9dd12bf66a554d6fe87c
Jideofor Nwoke <njusticej@gmail.com>
2020-01-23 10:38:05 +0100

    add persisting order state with local storage

--- src/components/App.js
+++ src/components/App.js
@@ -14,10 +14,19 @@ class App extends React.Component {
     }
     componentDidMount() {
         const { params } = this.props.match
+        //first reinstate our local storage
+        const localStorageRef = localStorage.getItem(params.storeId)
+        if(localStorageRef){
+            this.setState({order:JSON.parse(localStorageRef)})
+        }
         this.ref = base.syncState(`${params.storeId}/fishes`, {
             context: this,
             state: "fishes"
           });
+    }
+    componentDidUpdate(){
+        console.log("it updated")
+        localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
     }
 
     componentWillUnmount(){
