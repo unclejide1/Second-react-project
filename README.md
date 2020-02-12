f48c9e304e636495a7dc487be413ad48e9ab67cb
Jideofor Nwoke <njusticej@gmail.com>
2020-02-12 19:03:27 +0100

    add build

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Second-react-project
+  A store picker
---
+++ build/precache-manifest.97353f2dc6345895b904ecbb9c6d3559.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4ab83cc5a2aa2368d6fc8b6eec3370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cf6e57179a998ead819",
+    "url": "/static/css/main.887a147e.chunk.css"
+  },
+  {
+    "revision": "d6afb6fbcdb36b3ee4b6",
+    "url": "/static/js/2.17c41897.chunk.js"
+  },
+  {
+    "revision": "bedca72df8738a24c2ae0ce5bc65d601",
+    "url": "/static/js/2.17c41897.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3cf6e57179a998ead819",
+    "url": "/static/js/main.45966bb3.chunk.js"
+  },
+  {
+    "revision": "794cb3a239c16f167a95",
+    "url": "/static/js/runtime-main.080bbe98.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
